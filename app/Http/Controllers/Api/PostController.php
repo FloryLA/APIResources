1c1b9f01df35b74ac53de33ed031b15ee5818bde
FloryLA <sasukefmt93@gmail.com>
2021-03-04 20:05:33 -0600

    Creamos el controlador relacionando directamente con el modelo para ello de usa el siguente comado: php artisn make:controller Api/PostController --api --model=Post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 
-use App\Post;
+use App\Post; 
 use Illuminate\Http\Request;
 
 use App\Http\Requests\Post as PostRequest;
